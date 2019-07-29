da6aa8f514c0253791090392595ac24743413f2e
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-29 14:03:02 -0700

    installed eslint and all dependencies jeff, matt, tia

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./styles.scss";
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
