2241c97f0ef4dc6f790821c530ea19f1761fe615
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-29 15:57:52 -0700

    login work

---
+++ src/components/signin/signin.js
@@ -0,0 +1,75 @@
+import superagent from 'superagent';
+import React, { useState, useContext } from 'react';
+import { LoginContext } from './context.js';
+
+const API = process.env.REACT_APP_API;
+
+const If = props => {
+  return !!props.condition ? props.children : null;
+};
+
+/**
+ * login function
+ * @description logs a user in by contacting context to add their infor and get a token
+ */
+
+function Login() {
+  const context = useContext(LoginContext);
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  // const handleUsername = e => {
+  //   setUsername(e.target.value);
+  // };
+
+  // const handlePassword = e => {
+  //   setPassword(e.target.value);
+  // };
+
+  // const handleChange = e => {
+  //   if(e.target.name === 'username'){
+  //     setUsername(e.target.value);
+  //   }else{
+  //     setPassword(e.target.value);
+  //   }
+  // }
+
+  const handleSubmit = (e, loginMethodFromContext) => {
+    e.preventDefault();
+    superagent
+      .post(`${API}/signin`)
+      .auth(username, password)
+      .then(response => {
+        let token = response.text;
+        loginMethodFromContext(token);
+      })
+      .catch(console.error);
+  };
+  return (
+    <>
+      <If condition={context.loggedIn}>
+        <button onClick={context.logout}>Log Out</button>
+      </If>
+      <If condition={!context.loggedIn}>
+        <div>
+          <form onSubmit={e => handleSubmit(e, context.login)}>
+            <input
+              placeholder="username"
+              name="username"
+              onChange={e => setUsername(e.target.value)}
+            />
+            <input
+              placeholder="password"
+              name="password"
+              type="password"
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input type="submit" value="login" />
+          </form>
+        </div>
+      </If>
+    </>
+  );
+}
+
+export default Login;
