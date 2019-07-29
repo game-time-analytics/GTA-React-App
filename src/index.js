b1db0f44791cc6a487d14f03c7ba261520ecef10
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-29 14:41:42 -0700

    scaffolding additional files matt, tia, jeff

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/footer/footer';
 
 import './styles.scss';
 
 function App() {
   return (
-
-    <div className="App">
-      <h1>Game Time Analytics</h1>
-      <header>
-        <nav>
-          <ul>
-            <li>Player Stats</li>
-            <li>Admin</li>
-            <li>About Us</li>
-          </ul>
-        </nav>
-      </header>
-      <img src={ require('./assets/Football-Analytics-Limitations.jpg') } alt="GTA logo" />
-      <footer>2019 GTA Online &#169;</footer>
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Game Time Analytics</h1>
+        <header>
+          <nav>
+            <ul>
+              <li>Player Stats</li>
+              <li>Admin</li>
+              <li>About Us</li>
+            </ul>
+          </nav>
+        </header>
+        <img src={ require('./assets/Football-Analytics-Limitations.jpg') } alt="GTA logo" />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
