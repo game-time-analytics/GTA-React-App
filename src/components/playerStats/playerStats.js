2f16d8a80fb4091c85354da63880c682cad450a4
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-30 10:53:31 -0700

    player context work

--- src/components/playerStats/playerStats.js
+++ src/components/playerStats/playerStats.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { PlayerContext } from '../context/playerContext';
 
 /**
  * Home class that contains a hello world for when the home route is hit
  */
 const PlayerStats = () => {
+
+  const { state, dispatch } = useContext(PlayerContext);
+  // const [username, setUsername] = useState();
+  // const [password, setPassword] = useState();
+
+  useEffect(() => {
+    // console.log("update incoming");
+  }, []);
+
   /**
    * render function that returns a simple hello world
    */
