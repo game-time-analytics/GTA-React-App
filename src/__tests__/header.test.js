d052272e4652d0757df331b231bdfcae7e87ae4a
Matt Wilkin <mwilkin22@gmail.com>
2019-07-30 10:02:43 -0700

    add header test passing, jeff, tia, MATT.

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '../components/footer/footer';
 import { shallow } from 'enzyme';
 
-describe('footer component', () => {
+describe('header component', () => {
   it('should render as expected', () => {
-    const app = shallow(<footer></footer>);
-    expect(app.find('footer').exists()).toBeTruthy();
+    const app = shallow(<header></header>);
+    expect(app.find('header').exists()).toBeTruthy();
   });
 });
