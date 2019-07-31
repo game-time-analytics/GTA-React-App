4c8be5526c86d09b84cdb52e1ffa15c9d1c92d1c
Tia Rose <shortyshort789@aol.com>
2019-07-31 11:26:03 -0700

    added inline docs to playerStats, Tia, Matt, and Jeff

--- src/components/playerStats/playerStats.js
+++ src/components/playerStats/playerStats.js
@@ -1,9 +1,11 @@
+/* eslint-disable max-len */
 import React, { useState, useContext, useEffect } from 'react';
 import { PlayerContext } from '../context/playerContext';
 import MyForm from './form';
 
 /**
- * @description PlayerStats component search player by name to get their stats
+ * PlayerStats component
+ * @description sets the state for the players states (passing, touchdowns, interceptions)
  */
 
 const PlayerStats = () => {
@@ -19,6 +21,12 @@ const PlayerStats = () => {
     context.getPlayers();
   }, []);
 
+  /**
+   * @method handlePassing
+   * @param {Object} e
+   * @description calculates passing yards for the player the user selected and divides it by 16 and passes that calucation into setPassing
+   */
+
   const handlePassing = (e) => {
     e.preventDefault();
     const pYards = context.selectedPlayer.passing / 16;
@@ -26,6 +34,11 @@ const PlayerStats = () => {
     setPassing(yardEstimate);
   };
 
+  /**
+   * @method handleTouchdowns
+   * @param {Object} e 
+    * @description calculates touchdowns for the player the user selected and divides it by 16 and passes that calucation into setTouchdowns
+   */
   const handleTouchdowns = (e) => {
     e.preventDefault();
     const tDowns = context.selectedPlayer.touchdowns / 16;
@@ -33,16 +46,18 @@ const PlayerStats = () => {
     setTouchdowns(touchDownEstimate);
   };
 
+  /**
+   * @method handleInterceptions
+   * @param {Object} e 
+    * @description calculates interceptions for the player the user selected and divides it by 16 and passes that calucation into setInterceptions
+   */
+
   const handleInterceptions = (e) => {
     e.preventDefault();
     const interceptionsWeek = context.selectedPlayer.interceptions / 16;
     const interceptionEstimate = interceptionsWeek * e.target.value;
     setInterceptions(interceptionEstimate);
   };
-
-  /**
-   * render function that returns a simple hello world
-   */
 
   return (
       <React.Fragment>
