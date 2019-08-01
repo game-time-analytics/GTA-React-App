9d4c78ac9583b5ae842bbdc24a38a557486f9107
Tia Rose <shortyshort789@aol.com>
2019-08-01 12:08:12 -0700

    removed importing browser router in home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 /**
  * @description Home component that renders home and links to signin, signup and logo image
