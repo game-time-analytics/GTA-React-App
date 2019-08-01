d728a0dae9f945d40775ec8b30be3c1e121a0a62
Tia Rose <shortyshort789@aol.com>
2019-08-01 15:06:22 -0700

    have two footer tests passing

--- src/__tests__/footer/footer.test.js
+++ src/__tests__/footer/footer.test.js
@@ -1,10 +1,16 @@
+/* eslint-disable quote-props */
 import React from 'react';
-import '../../components/footer/footer';
 import { shallow } from 'enzyme';
+import Footer from '../../components/footer/footer';
 
 describe('footer component', () => {
-  it('should render as expected', () => {
-    const app = shallow(<footer></footer>);
+  it('should render a footer tag as expected', () => {
+    const app = shallow(<Footer />);
     expect(app.find('footer').exists()).toBeTruthy();
+  });
+
+  it('should render the correct link', () => {
+    const app = shallow(<Footer />);
+    expect(app.find('a').props('href')).toEqual({ 'children': 'GTA Online', 'href': 'https://github.com/game-time-analytics' });
   });
 });
