05d9867814a0c29c88c7e654081b1e5736f39e79
Matt Wilkin <mwilkin22@gmail.com>
2019-08-01 11:01:30 -0700

    add link to gta github.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+
 /**
  * @description Footer component that renders a basic footer with the app name and copyright info
  */
 
 const Footer = () => {
   return (
-    <footer>2019 GTA Online &#169;</footer>
+    <footer>
+      <a href="https://github.com/game-time-analytics">GTA Online</a> 2019 &#169;
+    </footer>
   );
 };
 
