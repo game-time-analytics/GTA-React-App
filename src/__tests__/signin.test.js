52788bd4d37dff0fe779601f98d81659b367973d
Tia Rose <shortyshort789@aol.com>
2019-08-01 14:55:00 -0700

    corrected some scaffolding tests in signin.test

--- src/__tests__/signin.test.js
+++ src/__tests__/signin.test.js
@@ -3,19 +3,20 @@
 // import { shallow } from 'enzyme';
 
 describe('signin component', () => {
-  // it('should not render if logged in', () => {
+  it('should not render if logged in', () => {
   // const contextValues = useContext(true);
   // jest
   //   .spyOn(Login, 'useLoginContext')
   //   .mockImplementation(() => contextValues);
 
-  //   const app = shallow(<Login context = {'true'}/>);
+    //   const app = shallow(<Login context = {'true'}/>);
 
   //   expect(app.find('form').exists()).toBeFalsy();
-  // });
+  });
 
   it('should not render a form if logged in', () => {
     expect('tia').toEqual('tia');
+
     // let context = {context.true};
     // const app = shallow(<Login />);
     // expect(app.find('form').exists()).toBeFalsy();
@@ -23,12 +24,13 @@ describe('signin component', () => {
 
   it('should render a form if not logged in', () => {
     expect('tia').toEqual('tia');
+
     // let context = {context.false};
     // const app = shallow(<Login />);
     // expect(app.find('form').exists()).toBeTruthy();
   });
 
-  // it('should render an input if not logged in', ()=>{
+  it('should render an input if not logged in', ()=>{
 
-  // });
+  });
 });
